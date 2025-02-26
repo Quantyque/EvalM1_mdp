632cbbe0e414f8b958594c90839d82b1a9b16e8d
Antoine RICHARD <95919133+Quantyque@users.noreply.github.com>
2025-02-26 15:32:11 +0100

    Formulaire de création d'application

--- EvalM1_Front_mdp/EvalM1_mdp/src/app/app.routes.ts
+++ EvalM1_Front_mdp/EvalM1_mdp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ApplicationComponent } from './application/application.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'application', pathMatch: 'full' },
+    { path: 'application',
+      loadComponent: () => import('./application/application.component').then(m => m.ApplicationComponent)
+    }
+];
--- EvalM1_Front_mdp/EvalM1_mdp/src/app/application/application.component.ts
+++ EvalM1_Front_mdp/EvalM1_mdp/src/app/application/application.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   selector: 'app-application',
-  imports: [],
   templateUrl: './application.component.html',
-  styleUrl: './application.component.scss'
+  styleUrls: ['./application.component.scss']
 })
-export class ApplicationComponent {
+export class ApplicationComponent implements OnInit {
+  applications: any[] = [];
+  
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.http.get<any[]>('/api/applications').subscribe(data => {
+      this.applications = data;
+    });
+  }
+
+  onSubmit(form: any): void {
+    if (form.valid) {
+      console.log('Form submitted!', form.value);
+    }
+  }
 }
